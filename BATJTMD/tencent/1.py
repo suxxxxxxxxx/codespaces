a856f92b38d400c9d672ef618b0e56b098796f2d
su-xiangzhen <2165833853@qq.com>
2023-10-26 21:28:19 +0800

    bem

---
+++ BATJTMD/tencent/1.py
@@ -0,0 +1,43 @@
+import openai
+openai.api_key ="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"  # openai 的sdk
+COMPLETION_MODEL = "text-davinci-003"  #模型常量 达芬奇 完成
+# 字符串模板
+# 换行
+# 描述细节需求
+# 分布去执行 
+# 输出的格式
+prompt = """
+Consideration product:工厂现货PVC充气青蛙夜市地摊热卖充气玩具发光蛙儿童水上玩具
+
+1. Compose human readable product title used on Amazon in english within 20 words
+2. Write 5 selling points for the products in Amazon 
+3. Evaluat a price range for this product in U.S
+
+Output the result in json format with three properties called title,
+selling_points and price_range
+"""
+# 定义函数
+def get_response(prompt):
+    completions = openai.Completion.create(
+        # 大模型很值钱
+        engine=COMPLETION_MODEL, # 模型
+        prompt=prompt, # 提示词
+        max_tokens=512, #省点
+        n = 1, # 1条
+        stop=None,
+        temperature=0.0   # 自由发挥度 0-2 
+    )
+    print(completions)
+    message = completions.choices[0].text # 
+    return message
+
+
+print(get_response(prompt))
+    
+
+
+# {
+#     title: "",
+#     selling_points: "",
+#     price_range: ,
+# }
--- BATJTMD/tencent/readme.md
+++ BATJTMD/tencent/readme.md
@@ -1,34 +1,20 @@
-#腾讯日
-代码新手的编程素养和JS功底
--代码的可读性高于一切
- 注释
--优化代码
-    统一风格 看开发文档
+#跨境电商超人
 
-1.JS面试题
-    codewars
-    刷题网站
-    写一个函数，完成某项功能
-    字符串拼接
+-调研一下市场，小孩玩具，义乌
+-translate AI 代码
+-定价 openai 定价(经济学家)
+-写文案 openai prompt engineer
 
-2.es6语法
+aigc 前端工程师初体验
 
-3.智商游戏
-    换一种写法
-    闯将进来 抗压
-
-#WeUI button组件
-从任务看到本质
-- ul >li
-- 按钮组件 WEUI 源码 写出来
-    60%的界面是不需要做的，UI框架提供通用组件
-    40%业务结合代码
-    微信 2年
-- 抽象  BEM 命名规范
-    -共性
-        button 共性 类名 .btn 基类 .weui-btn
-        项目代号-组件名 大项目
-    -差异
-        .weui-btn_primary
-        .weui-btn_danger
-        .weui-btn_info
+-AI应用
+    -基于大模型
+        OPENAI的达芬奇 擅长文本处理
+    -花销
+        tokens 算力
+        API_KEY
+    -prompt + openai 接口调用
+    -JSON 继续业务
+    
+AI应用
+-提示词工程 + openai sdk +langchain +vue

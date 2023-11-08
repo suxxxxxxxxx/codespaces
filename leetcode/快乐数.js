8ec546f0f202cc5abb38c5f393959117e1b809de
su-xiangzhen <2165833853@qq.com>
2023-11-09 00:17:32 +0800

    快乐数

---
+++ leetcode/快乐数.js
@@ -0,0 +1,12 @@
+ar isHappy = function(n) {
+    let slow = n
+    let fast =getNext(n)
+    while(fast !== 1&&fast !==slow){
+        slow = getNext(slow)
+        fast= getNext(getNext(fast))
+    }
+    return fast ===1
+};
+let getNext = function(n){
+     return n.toString().split('').map(i => i**2).reduce((a,b)=>a+b)
+}

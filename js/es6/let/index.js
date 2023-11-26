ace54131dfc6283061ef9f4e0a60e41cd1a8b1d1
su-xiangzhen <2165833853@qq.com>
2023-11-26 23:17:54 +0800

    es6

---
+++ js/es6/let/index.js
@@ -0,0 +1,17 @@
+// console.log(a);
+// var a  = 2
+
+// let a = 2;
+// if(true){
+//     console.log(a);
+//     let a = 3;
+// }
+// var a = 2;
+// var a = 1
+// console.log(a);
+const obj = {
+    name: 'zhangsan',
+    age: 18
+}
+obj.name = 'lisi';
+console.log(obj.name);

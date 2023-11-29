ff50759214e720e1bfd611fc94d8c21246d74f90
su-xiangzhen <2165833853@qq.com>
2023-11-29 23:07:20 +0800

    颜色分类

---
+++ leetcode/75.js
@@ -0,0 +1,15 @@
+var sortColors = function(nums) {
+    const len = nums.length 
+    let p = -1, q = nums.length
+    for(let i = 0;i <= q;i++){
+        if(nums[i] == 0){
+            [nums[i],nums[p+1]] = [nums[p+1],nums[i]]
+            p += 1
+        }
+        else if(nums[i] == 2){
+            [nums[i],nums[q-1]] = [nums[q-1],nums[i]]
+            q -= 1
+        }
+    }
+    return nums
+};

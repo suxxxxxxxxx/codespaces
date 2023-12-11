4a05ab7952e2f39d378f6ea544f8ce42ce8b02d4
su-xiangzhen <2165833853@qq.com>
2023-12-11 21:56:08 +0800

    饿了m1

---
+++ vue/vue-sell/README.md
@@ -0,0 +1,6 @@
+# 项目开发流程
+1.创建全局的css文件，添加reset.css
+
+#组件化
+1.import引入
+2.compoenents
---
+++ vue/vue-sell/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-header/>
+  
+  <router-view/>
+</template>
+
+<script>
+import Header from '@/components/header/Header.vue'
+export default {
+  components: {
+    'v-header': Header
+  }
+
+
+}
+
+</script>
+
+<style lang="less">
+
+</style>

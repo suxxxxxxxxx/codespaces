8a130fecb97fc5e173473167a5efee9a9cc36f87
su-xiangzhen <2165833853@qq.com>
2023-12-12 21:39:55 +0800

    vue-sell3

--- vue/vue-sell/src/App.vue
+++ vue/vue-sell/src/App.vue
@@ -1,20 +1,31 @@
-<template>
-  <v-header/>
-  
+<template> 
+  <v-header :seller="sellerData"></v-header>
+
   <router-view/>
 </template>
 
+
 <script>
 import Header from '@/components/header/Header.vue'
+import { getSeller } from '@/api'
 export default {
-  components: {
-    'v-header': Header
+  components:{
+    'v-header':Header
+  },
+  data(){
+    return {
+      sellerData:{}
+    }
+  },
+  created(){
+    getSeller().then(res => {
+      console.log(res);
+      this.sellerData = res
+    })
   }
-
-
 }
-
 </script>
+
 
 <style lang="less">
 
--- vue/vue-sell/src/components/header/Header.vue
+++ vue/vue-sell/src/components/header/Header.vue
@@ -1,36 +1,69 @@
 <template>
-    <div class="header">
+    <div class="header" @click="showDetail">
       <div class="content-wrapper">
         <div class="avatar">
-          <img src="http://static.galileo.xiaojukeji.com/static/tms/seller_avatar_256px.jpg" alt="">
+          <img :src="seller.avatar" alt="">
         </div>
         <div class="content">
           <div class="title">
             <span class="brand"></span>
-            <span class="name">店铺名称</span>
+            <span class="name">{{ seller.name }}</span>
           </div>
-          <div class="description">蜂鸟专送/38分钟送达</div>
-          <div class="support">
+          <div class="description">{{ seller.description }}/{{ seller.deliveryTime}}分钟送达</div>
+          <div class="support" v-if="seller.supports">
             <!--  -->
-            <span class="text">在线支付满28减5</span>
+            <support-icon size="1" :type="seller.supports[0].type" />
+            <span class="text">{{ seller.supports[0].description }}</span>
           </div>
         </div>
-        <div class="support-count">
-          <span class="count">5个</span>
-          <i class="iconfont">></i>
+        <div class="support-count" v-if="seller.supports">
+          <span class="count">{{ seller.supports.length }}个</span>
+          <i class="iconfont icon-arrow-right" ></i>
         </div>
       </div>
   
       <div class="bulletin-wrapper">
-  
+        <span class="bulletin-title"></span>
+        <span class="bulletin-text">{{ seller.bulletin }}</span>
+        <i class="iconfont icon-arrow-right" ></i>
       </div>
+
+      <div class="bg" v-if="seller.avatar" :style="{backgroundImage: `url(${seller.avatar})`}">
+
+      </div>
+      <header-detail v-show="detailShow" @hide="handle"></header-detail>
     </div>
   </template>
   
   <script>
+  import SupportIcon from '@/components/support-icon/Support-icon.vue'
+  import HeaderDetail from '@/components/header-detail/Header-detail.vue'
     export default {
-      
+      components: {
+        SupportIcon,
+        HeaderDetail
+      },
+    props:{
+      seller:{
+        type:Object,
+        default:() => {}
+      }
+    },
+    data(){
+      return{
+        detailShow:false
+      }
+    },
+    methods:{
+      showDetail(){
+        this.detailShow = true
+      },
+      handle(val){
+        this.detailShow = val
+      }
     }
+  }
+  
   </script>
   
   <style lang="less" scoped>
@@ -38,6 +71,7 @@
   @import '@/common/style/mixin.less';
   .header{
     background: @color-background-ss;
+    position: relative;
     .content-wrapper{
       display: flex;
       padding: 24px 12px 18px 24px;
@@ -46,6 +80,7 @@
         width: 64px;
         height: 64px;
         img{
+          
           width: 100%;
           border-radius: 2px;
         }
@@ -80,6 +115,7 @@
           align-items: center;
           .text{
             font-size: @fontsize-small-s;
+            margin-left: 4px;
           }
         }
       }
@@ -105,6 +141,43 @@
         }
       }
     }
-  
+    .bulletin-wrapper{
+      display: flex;
+      height: 28px;
+      padding: 0 8px;
+      background-color: @color-background-sss;
+      align-items: center;
+      color: @color-white;
+      .bulletin-title{
+        // width: 22px;
+        flex:0 0 22px;
+        height: 12px;
+        .bg-image('bulletin');
+        background-size: 100% 100%;
+      }
+      .bulletin-text{
+        flex: 1;
+        font-size: @fontsize-small-s;
+        margin-left: 4px;
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
+      }
+      .icon-arrow-right{
+        font-size: @fontsize-small-s;
+        margin-left: 2px;
+      }
+    }
+    .bg{
+      position: absolute;
+      left: 0;
+      right: 0;
+      top: 0;
+      bottom: 0;
+      // background-image: url(http://static.galileo.xiaojukeji.com/static/tms/seller_avatar_256px.jpg);
+      background-size: 100% 100%;
+      filter: blur(10px);
+      z-index: -1;
+    }
   }
   </style>

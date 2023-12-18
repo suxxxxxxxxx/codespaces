ec925bf3ad1fc8de467c427a2ec1cdaf77895839
suxxxxxxxxx <2165833853@qq.com>
2023-12-18 22:49:55 +0800

    和为k的子数组

---
+++ leetcode/560.js
@@ -0,0 +1,14 @@
+var subarraySum = function(nums, k) {
+    let count = 0;
+    let sum = 0;
+    let map = new Map();
+    map.set(0,1)
+    for (let i = 0; i < nums.length; i++) {
+        sum+=nums[i];
+        if(map.has(sum-k)){
+            count+=map.get(sum-k)  
+        }
+        map.set(sum,(map.get(sum)||0)+1)
+    }
+    return count;
+}

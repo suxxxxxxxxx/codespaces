85341c1e25847c70a4c08f3e9a8b68b0262d51b4
suxxxxxxxxx <2165833853@qq.com>
2023-12-31 22:57:29 +0800

    二叉搜索树

---
+++ leetcode/98.js
@@ -0,0 +1,18 @@
+var isValidBST = function(root) {
+    let stack = [];
+    let inorder = -Infinity;
+    
+    while (stack.length || root) {
+        while (root) {
+        stack.push(root);
+        root = root.left;
+        }
+    
+        root = stack.pop();
+        if (root.val <= inorder) return false;
+        inorder = root.val;
+        root = root.right;
+    }
+    
+    return true;
+}

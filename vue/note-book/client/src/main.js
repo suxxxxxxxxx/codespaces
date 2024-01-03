c5d31d1a1dd746ac000e001f338476829208510b
suxxxxxxxxx <2165833853@qq.com>
2024-01-03 16:34:44 +0800

    vue+node+mysql01

---
+++ vue/note-book/client/src/main.js
@@ -0,0 +1,19 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import 'amfe-flexible' // 自动设置根字体大小
+import './assets/style/reset.css'
+
+import { Button, Form, Field, CellGroup } from 'vant';
+import 'vant/lib/index.css';
+
+const app = createApp(App);
+app.use(router);
+
+// 3. 注册你需要的组件
+app.use(Button);
+app.use(Form);
+app.use(Field);
+app.use(CellGroup);
+
+app.mount('#app')

cccd1c83f18173b5b74000ad55ad58a0437b2555
suxxxxxxxxx <2165833853@qq.com>
2024-01-05 21:57:54 +0800

    最小花费爬楼梯

---
+++ leetcode/746.js
@@ -0,0 +1,7 @@
+var minCostClimbingStairs = function(cost) {
+    let dp = [0, 0];
+    for (let i = 2; i <= cost.length; i++) {
+        dp[i] = Math.min(dp[i - 2] + cost[i - 2], dp[i - 1] + cost[i - 1]);
+    }
+    return dp[cost.length];
+}

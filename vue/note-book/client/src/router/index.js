a6ad1bd8130b6a3fa0a13fb0c5d9227b9e2c3d46
suxxxxxxxxx <2165833853@qq.com>
2024-01-05 16:05:30 +0800

    修改前端

--- vue/note-book/client/src/router/index.js
+++ vue/note-book/client/src/router/index.js
@@ -1,47 +1,50 @@
-import { createRouter, createWebHistory } from 'vue-router'
-
+import {createRouter,createWebHistory} from 'vue-router'
 const routes = [
-  {
-    path: '/login',
-    name: 'login',
-    component: () => import('../views/Login.vue'),
-    meta:{
-      title:'登录'
+    {
+        path:'/login',
+        name:'login',
+        component:() => import('../views/Login.vue'),
+        meta: {
+            title:'登录'
+        }
+    },
+    {
+        path:'/register',
+        name:'register',
+        component:() => import('../views/Register.vue'),
+        meta: {
+            title:'注册'
+        }
+    },
+    {
+        path:'/NoteClass',
+        name:'noteClass',
+        component:() => import('../views/NoteClass.vue'),
+        meta: {
+            title:'笔记分类'
+        }
     }
-  },
-  {
-    path: '/register',
-    name: 'register',
-    component: () => import('../views/Register.vue'),
-    meta:{
-      title:'注册'
-    }
-  },
-  {
-    path:'/noteclass',
-    name:'noteclass',
-    component: () => import('../views/NoteClass.vue'),
-  }
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes
+    history: createWebHistory(),
+    routes
 })
 
-//路由守卫
-const writePath = ['/login','/register']
-router.bdforeEach(to,from,next) => {
-  document.title = to.meta.title
-  if(whitePath.includes(to.path)){//想要去详情页
-    if(!sessionStorage.getItem('userInfo')){//没登陆
-      router.push('/login')
-      return 
+// 路由守卫
+const whitePath = ['/login','/register']
+router.beforeEach((to,from,next) => {
+    document.title = to.meta.title
+
+    if(!whitePath.includes(to.path)) { // 你想去详情页
+        if(!sessionStorage.getItem('userInfo')){ // 没登入
+            router.push('./login')
+            return
+        }
+        next()
+        return 
     }
     next()
-    return
-  }
-  next()
-}
+})
 
 export default router

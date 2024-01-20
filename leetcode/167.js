72ce85e4724113ab6e1cc7b8c1700bb1d9a1a386
suxxxxxxxxx <2165833853@qq.com>
2024-01-20 22:14:02 +0800

    两数之和 II - 输入有序数组

---
+++ leetcode/167.js
@@ -0,0 +1,10 @@
+var twoSum = function(numbers, target) {
+    let left = 0, right = numbers.length - 1;
+    while (left < right) {
+        let sum = numbers[left] + numbers[right];
+        if (sum === target) return [left + 1, right + 1];
+        if (sum < target) left++;
+        else right--;
+    }
+    return [-1, -1];
+}

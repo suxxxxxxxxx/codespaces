61b0d03a492bcdfe916e0c4e7055aa4b0bcfb85f
suxxxxxxxxx <2165833853@qq.com>
2024-01-21 15:03:20 +0800

    插入区间

---
+++ leetcode/57.js
@@ -0,0 +1,19 @@
+var insert = function(intervals, newInterval) {
+    let res = [];
+    let i = 0;
+    while (i < intervals.length && intervals[i][1] < newInterval[0]) {
+        res.push(intervals[i]);
+        i++;
+    }
+    while (i < intervals.length && intervals[i][0] <= newInterval[1]) {
+        newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+        newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+        i++;
+    }
+    res.push(newInterval);
+    while (i < intervals.length) {
+        res.push(intervals[i]);
+        i++;
+    }
+    return res;
+}

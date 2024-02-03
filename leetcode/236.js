f033154af794fccbe969c0df37fc1e25fa9cb6a8
suxxxxxxxxx <2165833853@qq.com>
2024-02-03 23:36:18 +0800

    二叉树最近公共祖先

---
+++ leetcode/236.js
@@ -0,0 +1,8 @@
+var lowestCommonAncestor = function(root, p, q) {
+    if (root === null || root === p || root === q) return root;
+    let left = lowestCommonAncestor(root.left, p, q);
+    let right = lowestCommonAncestor(root.right, p, q);
+    if (left === null) return right;
+    if (right === null) return left;
+    return root;
+}

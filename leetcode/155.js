e6dce9740ed03a26fef81466bbdb64ed4e6cb7bb
suxxxxxxxxx <2165833853@qq.com>
2024-02-07 16:55:55 +0800

    最小栈

---
+++ leetcode/155.js
@@ -0,0 +1,21 @@
+var MinStack = function() {
+    this.stack = [];
+    this.minStack = [];
+}
+MinStack.prototype.push = function(val) {
+    this.stack.push(val);
+    if (this.minStack.length === 0 || val <= this.minStack[this.minStack.length - 1]) {
+        this.minStack.push(val);
+    }
+}
+MinStack.prototype.pop = function() {
+    if (this.stack.pop() === this.minStack[this.minStack.length - 1]) {
+        this.minStack.pop();
+    }
+}
+MinStack.prototype.top = function() {
+    return this.stack[this.stack.length - 1];
+}
+MinStack.prototype.getMin = function() {
+    return this.minStack[this.minStack.length - 1];
+}
